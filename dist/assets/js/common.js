7c32d0795ab7ebf9362e2bab2c35862ab099aa33
Fromaline <grechino2412@gmail.com>
2020-07-03 21:49:34 +0300

    add a lot of stuff

--- dist/assets/js/common.js
+++ dist/assets/js/common.js
@@ -1,35 +1,38 @@
 $(document).ready(function () {
   $(document).on('modal-opens', (e) => {
     const modal = e.detail.modal
-    $(modal)
-      .find('.carousel')
-      .slick({
-        infinite: true,
-        dots: false,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        prevArrow: $('.carousel-arrow_left'),
-        nextArrow: $('.carousel-arrow_right'),
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
+
+    if ($(modal).find('.carousel').length > 0) {
+      $(modal)
+        .find('.carousel')
+        .slick({
+          infinite: true,
+          dots: false,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          prevArrow: $('.carousel-arrow_left'),
+          nextArrow: $('.carousel-arrow_right'),
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+              },
             },
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              },
             },
-          },
-          {
-            breakpoint: 500,
-            settings: {
-              slidesToShow: 1,
+            {
+              breakpoint: 500,
+              settings: {
+                slidesToShow: 1,
+              },
             },
-          },
-        ],
-      })
+          ],
+        })
+    }
   })
 })
