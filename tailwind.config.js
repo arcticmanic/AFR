1e87adf1f3302a09c37e55e76f5b535d7b90a83e
Fromaline <grechino2412@gmail.com>
2020-07-04 17:46:30 +0300

    add new page

--- tailwind.config.js
+++ tailwind.config.js
@@ -178,14 +178,14 @@ module.exports = {
       // ],
     },
     fontSize: {
-      xxs: '0.6875rem',
-      xs: '0.75rem',
-      tiny: '0.8125rem',
-      sm: '0.875rem',
-      base: '0.9375rem',
-      md: '1.0625rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
+      xxs: '0.6875rem', // 11px
+      xs: '0.75rem', // 12px
+      tiny: '0.8125rem', // 13px
+      sm: '0.875rem', // 14px
+      base: '0.9375rem', // 15px
+      md: '1.0625rem', // 17px
+      lg: '1.125rem', // 18px
+      xl: '1.25rem', // 20px
       '2xl': '1.5rem',
       '3xl': '1.75rem',
       '3-5xl': '1.875rem',
