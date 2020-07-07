a97e35fb9e6608fdbeb35bf584999feb796ec4cf
Fromaline <grechino2412@gmail.com>
2020-07-07 15:17:25 +0300

    fixies and addongs

--- dist/assets/js/common.js
+++ dist/assets/js/common.js
@@ -2,16 +2,66 @@ $(document).ready(function () {
   $(document).on('modal-opens', e => {
     const modal = e.detail.modal
 
+    const carouselBaseOptions = {
+      infinite: true,
+      dots: false,
+      prevArrow: $('.carousel-arrow_left'),
+      nextArrow: $('.carousel-arrow_right'),
+    }
+
     if ($(modal).find('.carousel').length > 0) {
       $(modal)
         .find('.carousel')
         .slick({
-          infinite: true,
-          dots: false,
           slidesToShow: 5,
           slidesToScroll: 1,
-          prevArrow: $('.carousel-arrow_left'),
-          nextArrow: $('.carousel-arrow_right'),
+          ...carouselBaseOptions,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 500,
+              settings: {
+                slidesToShow: 1,
+              },
+            },
+          ],
+        })
+    }
+
+    if (
+      $(modal).find('.carousel-nav').length > 0 &&
+      $(modal).find('.carousel-for-nav').length > 0
+    ) {
+      $(modal).find('.carousel-for-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        asNavFor: '.carousel-nav',
+        infinite: true,
+        dots: false,
+      })
+
+      $(modal)
+        .find('.carousel-nav')
+        .slick({
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          centerMode: true,
+          focusOnSelect: true,
+          asNavFor: '.carousel-for-nav',
           responsive: [
             {
               breakpoint: 1024,
