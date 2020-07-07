bc09ee871cedb09be009b175416a4c162ac7378e
Fromaline <grechino2412@gmail.com>
2020-07-07 17:16:09 +0300

    fixies and addons

--- src/js/lazyLoading.js
+++ src/js/lazyLoading.js
@@ -2,7 +2,7 @@ import LazyLoad from 'vanilla-lazyload'
 
 const lazyLoading = () => {
   const lazyLoadInstance = new LazyLoad({
-    elements_selector: '.lazy',
+    elements_selector: 'img[data-src]',
     use_native: true,
   })
 
